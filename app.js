b651f42f5a2162808745bbc0692733cc57943587
Ayaanshaikh12243 <Ayaansalman74@gmail.com>
2025-09-05 15:31:15 +0530

    port 80

--- app.js
+++ app.js
@@ -28,4 +28,4 @@ app.get("/", (req, res) => {
 });
 //Server check
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(80, () => console.log("Server running on port 3000"));
